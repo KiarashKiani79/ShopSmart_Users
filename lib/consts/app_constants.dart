8ef6eb97c9e144bf06181862fcc14c94e0332c32
Kiarash Kiani <kiarash.kiani7997@gmail.com>
2023-09-14 19:04:09 +0430

    Create the Categories widget in the Home Screen

--- lib/consts/app_constants.dart
+++ lib/consts/app_constants.dart
@@ -1,3 +1,4 @@
+import '../models/categories_model.dart';
 import '../services/assets_manager.dart';
 
 class AppConstants {
@@ -7,5 +8,48 @@ class AppConstants {
   static List<String> bannersImages = [
     AssetsManager.banner1,
     AssetsManager.banner2
+  ];
+
+  static List<CategoriesModel> categoriesList = [
+    CategoriesModel(
+      id: "Phones",
+      image: AssetsManager.mobiles,
+      name: "Phones",
+    ),
+    CategoriesModel(
+      id: "Laptops",
+      image: AssetsManager.pc,
+      name: "Laptops",
+    ),
+    CategoriesModel(
+      id: "Electronics",
+      image: AssetsManager.electronics,
+      name: "Electronics",
+    ),
+    CategoriesModel(
+      id: "Watches",
+      image: AssetsManager.watch,
+      name: "Watches",
+    ),
+    CategoriesModel(
+      id: "Clothes",
+      image: AssetsManager.fashion,
+      name: "Clothes",
+    ),
+    CategoriesModel(
+      id: "Shoes",
+      image: AssetsManager.shoes,
+      name: "Shoes",
+    ),
+    CategoriesModel(
+      id: "Books",
+      image: AssetsManager.book,
+      name: "Books",
+    ),
+    CategoriesModel(
+      id: "Cosmetics",
+      image: AssetsManager.cosmetics,
+      name: "Cosmetics",
+    ),
   ];
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,7 @@ import '../consts/theme_data.dart';
 import '../providers/theme_provider.dart';
 import '../services/assets_manager.dart';
 import '../widgets/app_name_text.dart';
+import '../widgets/products/ctg_rounded_widget.dart';
 import '../widgets/products/latest_arrival.dart';
 import '../widgets/title_text.dart';
 
@@ -18,6 +19,7 @@ class HomeScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
     return Scaffold(
+      // appBar
       appBar: AppBar(
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -28,59 +30,86 @@ class HomeScreen extends StatelessWidget {
         title: const AppNameTextWidget(),
         systemOverlayStyle: statusBarTheme(themeProvider),
       ),
+      // body
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 15,
-            ),
-            SizedBox(
-              height: size.height * 0.25,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(25),
-                child: Swiper(
-                  autoplay: true,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                        AppConstants.bannersImages[index],
-                        fit: BoxFit.fill,
-                      ),
-                    );
-                  },
-                  itemCount: AppConstants.bannersImages.length,
-                  pagination: const SwiperPagination(
-                    // alignment: Alignment.center,
-                    builder: DotSwiperPaginationBuilder(
-                        activeColor: Colors.red,
-                        color: Color.fromARGB(255, 255, 202, 202)),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 15,
+              ),
+              // banners swiper
+              SizedBox(
+                height: size.height * 0.25,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(25),
+                  child: Swiper(
+                    autoplay: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          AppConstants.bannersImages[index],
+                          fit: BoxFit.fill,
+                        ),
+                      );
+                    },
+                    itemCount: AppConstants.bannersImages.length,
+                    pagination: const SwiperPagination(
+                      // alignment: Alignment.center,
+                      builder: DotSwiperPaginationBuilder(
+                          activeColor: Colors.red,
+                          color: Color.fromARGB(255, 255, 202, 202)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 8),
-              child: TitlesTextWidget(label: "Latest arrival"),
-            ),
-            const SizedBox(
-              height: 5.0,
-            ),
-            SizedBox(
-              height: size.height * 0.2,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return const LatestArrivalProductsWidget();
-                  }),
-            )
-          ],
+              const SizedBox(
+                height: 15.0,
+              ),
+              // Latest arrival title
+              const Padding(
+                padding: EdgeInsets.only(left: 8),
+                child: TitlesTextWidget(label: "Latest arrival"),
+              ),
+              const SizedBox(
+                height: 5.0,
+              ),
+              // Latest arrival products
+              SizedBox(
+                height: size.height * 0.2,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return const LatestArrivalProductsWidget();
+                    }),
+              ),
+              // Categories title
+              const Padding(
+                padding: EdgeInsets.only(left: 8),
+                child: TitlesTextWidget(label: "Categories"),
+              ),
+              const SizedBox(
+                height: 13.0,
+              ),
+              // Categories list
+              GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 4,
+                children:
+                    List.generate(AppConstants.categoriesList.length, (index) {
+                  return CategoryRoundedWidget(
+                    image: AppConstants.categoriesList[index].image,
+                    name: AppConstants.categoriesList[index].name,
+                  );
+                }),
+              ),
+            ],
+          ),
         ),
       ),
     );
