1dfbe0f52687cd7f4c04bed684c79656d9bb440f
Kiarash Kiani <kiarash.kiani7997@gmail.com>
2023-09-20 16:54:49 +0430

    Sign-in with Google

--- lib/widgets/auth/google_btn.dart
+++ lib/widgets/auth/google_btn.dart
@@ -1,8 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ionicons/ionicons.dart';
+
+import '../../root_screen.dart';
+import '../../services/my_app_functions.dart';
 
 class GoogleButton extends StatelessWidget {
   const GoogleButton({super.key});
+  Future<void> _signInWithGoogle({required BuildContext context}) async {
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      await FirebaseAuth.instance.signInWithCredential(credential);
+
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        Navigator.pushReplacementNamed(context, RootScreen.routName);
+      });
+      Fluttertoast.showToast(
+        msg: "Logged in successfully!",
+        backgroundColor: Colors.blue,
+      );
+    } on FirebaseException catch (error) {
+      await MyAppFunctions.showErrorOrWarningDialog(
+        context: context,
+        subtitle: error.message.toString(),
+        fct: () {},
+      );
+    } catch (error) {
+      await MyAppFunctions.showErrorOrWarningDialog(
+        context: context,
+        subtitle: error.toString(),
+        fct: () {},
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,11 +55,6 @@ class GoogleButton extends StatelessWidget {
       style: ElevatedButton.styleFrom(
         elevation: 1,
         padding: const EdgeInsets.all(12.0),
-        // shape: RoundedRectangleBorder(
-        //   borderRadius: BorderRadius.circular(
-        //     12.0,
-        //   ),
-        // ),
       ),
       icon: const Icon(
         Ionicons.logo_google,
@@ -23,7 +63,9 @@ class GoogleButton extends StatelessWidget {
       label: const Text(
         "Sign in with Google",
       ),
-      onPressed: () async {},
+      onPressed: () async {
+        await _signInWithGoogle(context: context);
+      },
     );
   }
 }
